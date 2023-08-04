e12635fe47e03b1d616c744d799b97b75569a389
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 17:06:24 +0000

    Up4PM6:06 PMe4

--- app/components/testimonials_comp.js
+++ app/components/testimonials_comp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import USER1 from "../public/assets/images/testimonials_img_1.png"
+import USER1 from "../public/assets/images/testimonials_img_1.png";
 
 const Testimonials = () => {
   {
