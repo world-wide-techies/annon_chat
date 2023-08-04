c549518c51dd93b156dcaf97631bd96d0566cdef
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 17:45:04 +0000

    Up4PM6:45 PMe4

--- app/components/testimonials_comp.js
+++ app/components/testimonials_comp.js
@@ -8,7 +8,7 @@ const Testimonials = () => {
     <>
       <div className="w-[1440px] h-[1078px] relative bg-gradient-to-b from-indigo-500 to-indigo-800">
         <div className="w-[641.11px] h-[930.76px] left-[123.95px] top-[-29.76px] absolute"></div>
-        <div className="h-[730px] py-2 left-[205px] top-[227px] absolute flex-col justify-start items-start gap-12 inline-flex">
+        <div className="h-[730px] py-2 left-[0px] top-[227px] absolute flex-col justify-start items-start gap-12 inline-flex">
           <div className="self-stretch h-[195px] flex-col justify-center items-end gap-2 flex">
             <div className="self-stretch shadow justify-end items-center inline-flex">
               <div className="w-[673px] self-stretch px-3 py-4 bg-gradient-to-r from-fuchsia-800 to-pink-500 rounded-[11px] flex-col justify-start items-end inline-flex">
