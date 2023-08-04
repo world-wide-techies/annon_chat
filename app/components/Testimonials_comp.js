f546e7217071f1cbd517d08a0fd8af4c1305b961
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 16:44:57 +0000

    Up4PM5:44 PMe4

--- app/components/Testimonials_comp.js
+++ app/components/Testimonials_comp.js
@@ -1,24 +1,11 @@
 
 
-import React from 'react'
-
-function Testimonials_comp() {
-  return (
-    <div>Testimonials_comp</div>
-  )
-}
-
-export default Testimonials_comp
-
-
-
-
 {
-// &quot;AnonChat is a game-changer! I use it for confidential
-//     work discussions with my team members, and it&apos;s been a
-//     breeze. The anonymity adds an extra layer of comfort, and we
-//     can brainstorm ideas without any inhibitions. Plus, the chat
-//     disappearing after we&apos;re done? Genius! Highly recommend
-//     it for anyone looking for secure and spontaneous
-//     conversations. 🙌&quot;
+  // &quot;AnonChat is a game-changer! I use it for confidential
+  //     work discussions with my team members, and it&apos;s been a
+  //     breeze. The anonymity adds an extra layer of comfort, and we
+  //     can brainstorm ideas without any inhibitions. Plus, the chat
+  //     disappearing after we&apos;re done? Genius! Highly recommend
+  //     it for anyone looking for secure and spontaneous
+  //     conversations. 🙌&quot;
 }
