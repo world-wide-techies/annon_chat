df219711f93281220c7da981132ee37f7b0ab965
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 16:22:13 +0000

    Up4PM5:22 PMe4

--- app/components/Testimonials_comp.js
+++ app/components/Testimonials_comp.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Testimonials_comp = () => {
-    {&quot;AnonChat is a game-changer! I use it for confidential
+    {"&quot;AnonChat is a game-changer! I use it for confidential
         work discussions with my team members, and it&apos;s been a
         breeze. The anonymity adds an extra layer of comfort, and we
         can brainstorm ideas without any inhibitions. Plus, the chat
         disappearing after we&apos;re done? Genius! Highly recommend
         it for anyone looking for secure and spontaneous
-        conversations. 🙌&quot;}
+        conversations. 🙌&quot;"}
  
  return <div>
 
