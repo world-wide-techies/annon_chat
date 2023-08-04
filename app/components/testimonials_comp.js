a4733a7500bc97578d971374cf46fe655795b2ad
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 17:34:52 +0000

    Up4PM6:34 PMe4

--- app/components/testimonials_comp.js
+++ app/components/testimonials_comp.js
@@ -2,6 +2,7 @@ import React from "react";
 import USER1 from "@/public/assets/images/testimonials_img_1.png";
 import USER2 from "@/public/assets/images/testimonials_img_2.png";
 import Image from "next/image";
+import CHECKER 
 const Testimonials = () => {
   {
     // &quot;AnonChat is a game-changer! I use it for confidential
