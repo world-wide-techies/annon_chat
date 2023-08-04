9d646c5f4d9df551816366f73c7b07a16679a453
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-04 21:26:56 +0000

    Up4PM10:26 PMe4

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ import Testimonials from './components/testimonials_comp'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   
+   
+   <main className="flex min-h-screen flex-col items-center justify-between p-24">
     
    
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
