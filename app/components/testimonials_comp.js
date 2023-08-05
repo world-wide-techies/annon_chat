333fee53ab2ae5c1750f5d22cf4c39db0b6c5fc0
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-05 11:11:03 +0000

    Up5PM12:11 PMe5

--- app/components/testimonials_comp.js
+++ app/components/testimonials_comp.js
@@ -10,9 +10,9 @@ const Testimonials = () => {
   return (
     <div>
       <div className="w-full h-full relative bg-gradient-to-b from-indigo-500 to-indigo-800">
-        <div className="flex flex-col h-screen w-screen justify-center place-items-center">
-          <div className="h-[195px] flex-col justify-center items-start flex">
-            <div className="w-[673px] relative self-stretch px-3 py-4 bg-gradient-to-r from-fuchsia-800 to-pink-500 rounded-[11px] ">
+        <div className="flex flex-col h-screen w-screen justify-center items-center">
+          <div className="h-[195px] flex-col justify-center flex">
+            <div className="w-[673px] relative  px-3 py-4 bg-gradient-to-r from-fuchsia-800 to-pink-500 rounded-[11px] ">
               <div className="w-[11px] h-5 right-[-5px] bottom-[-1px] absolute">
                 <Image
                   src={CONTAINER1}
