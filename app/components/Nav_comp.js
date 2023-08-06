201f7c3b726dff43ed4918a87c2a2272867fdeec
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-06 23:08:24 +0000

    Monday, August 7, 2023 at 12:08 AM West Africa Standard Time

--- app/components/Nav_comp.js
+++ app/components/Nav_comp.js
@@ -4,17 +4,17 @@ const Nav = () => {
   return (
     <div>
       <div className="w-[1440px] h-[86px] px-[100px] py-4 bg-white bg-opacity-5 shadow justify-between items-center gap-2 inline-flex">
-        <div className="justify-center items-center gap-2 flex">
+       
           <div className="text-center text-white-100 text-[40px] font-medium leading-[54px]">
             AnonChat!
           </div>
-        </div>
+    
         <div className="px-8 py-3 bg-white rounded-[109px] shadow-inner justify-center items-center gap-2.5 flex">
-          <div className="w-6 h-6 relative" />
+          
           <div className="text-indigo-500 text-xl font-normal leading-7">
             Start a Chat
           </div>
-        </div>
+       
       </div>
     </div>
   );
