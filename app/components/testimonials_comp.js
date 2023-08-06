a850e640e01950f2b252379a2fa6ecd6ec18715a
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-06 07:26:27 +0000

    Up6AM8:26 AMe6

--- app/components/testimonials_comp.js
+++ app/components/testimonials_comp.js
@@ -106,7 +106,7 @@ const Testimonials = () => {
         <div className="w-[887px] left-1/2 transform -translate-x-1/2 top-[100px] absolute text-center text-white text-5xl font-medium leading-[54px]">
           What Our Users Are Saying!
         </div>
-        <div className="w-[558px] h-[558px] right-0 top-[140px] absolute">
+        <div className="w-[558px] h-[558px]  bottom-0 ml-[-10px] absolute">
           <Image src={USER2} alt="" className="w-[500px] h-[558px]" />
         </div>
       </div>
