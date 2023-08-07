5dd9b6cc51f6b0af46ff1697bec09be76c8b3071
Edidiong <etokedidionge@gmail.com>
2023-08-07 18:04:17 +0100

    change from arrow function to regular function

--- app/components/Footer_comp.js
+++ app/components/Footer_comp.js
@@ -2,31 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Testimonial = () => {
+function Footer() {
   return (
-    <div className="bg-alpha-white-90">  
-    <div className="w-full px-12 md:flex items-center justify-between font-roboto text-sm font-semibold  mt-10 text-transparent bg-clip-text bg-gradient-to-b from-[#755BDF] to-[#2C1FA3]">
-      <div className="leading-5 text-center md:text-left">
-        © {new Date().getFullYear()} AnonChat. All rights reserved.
-      </div>
+    <footer className="bg-alpha-white-90">
+      <div className="w-full px-12 md:flex items-center justify-between font-roboto text-sm font-semibold  mt-10 text-transparent bg-clip-text bg-gradient-to-b from-[#755BDF] to-[#2C1FA3]">
+        <div className="leading-5 text-center md:text-left">
+          © {new Date().getFullYear()} AnonChat. All rights reserved.
+        </div>
 
-      <div className="flex items-center justify-center leading-5">
-        <div className="flex space-x-4">
-          <Link href="/">Privacy Policy</Link>
-          <Link href="/">Terms and Conditions</Link>
-        </div>
-        <div className="hidden md:block -mt-8 -ml-8">
-          <Image
-            src="/assets/images/footer_img_1.png"
-            alt="hand img"
-            width={100}
-            height={100}
-          />
+        <div className="flex items-center justify-center leading-5">
+          <div className="flex space-x-4">
+            <Link href="/">Privacy Policy</Link>
+            <Link href="/">Terms and Conditions</Link>
+          </div>
+          <div className="hidden md:block -mt-8 -ml-8">
+            <Image
+              src="/assets/images/footer_img_1.png"
+              alt="hand img"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
       </div>
-    </div>
-    </div>
+    </footer>
   );
-};
+}
 
-export default Testimonial;
+export default Footer;
