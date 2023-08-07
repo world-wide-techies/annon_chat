06018d2ae6e40a3a7408fb24a32c76ae48c24530
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-07 12:53:33 +0000

    Monday, August 7, 2023 at 1:53 PM West Africa Standard Time

--- app/page.js
+++ app/page.js
@@ -4,9 +4,8 @@ import Testimonials from "./components/testimonials_comp";
 
 export default function Home() {
   return (
-    <div className="bg-testimonials_bg">
-      <Nav />
-      <Testimonials />
+    <div >
+     
     </div>
   );
 }
