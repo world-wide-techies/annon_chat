a08a5810a774c674e386945afb5fb4fd8a04a8aa
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-08 13:00:50 +0000

    Tuesday, August 8, 2023 at 2:00 PM West Africa Standard Time

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Footer from "./components/Footer_comp";
+import HowItWorks from "./components/howItWorks_comp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Footer />
-      </div>
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
