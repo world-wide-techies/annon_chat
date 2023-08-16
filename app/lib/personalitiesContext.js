4d460a187d15d0b74c2e4f711c12e67031c7bd0a
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-16 13:32:32 +0100

    created personality context

---
+++ app/lib/personalitiesContext.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useContext, createContext } from "react";
+
+export const PersonalityContext = createContext();
+
+export const PersonalityContextProvider = ({ children }) => {
+  const [personality, setPersonality] = useState({});
+
+  return (
+    <PersonalityContext.Provider
+      value={{
+        personality,
+        setPersonality,
+      }}
+    >
+      {children}
+    </PersonalityContext.Provider>
+  );
+};
+
+export const personalityContext = () => {
+  return personalityContext(SessionContext);
+};
