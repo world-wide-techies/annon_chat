522964bb316e7ab41dbb20645c3f1a5e5a2af3c0
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-21 17:12:45 +0000

    Monday, August 21, 2023 at 6:12 PM West Africa Standard Time

--- app/components/Avatar_comp.js
+++ app/components/Avatar_comp.js
@@ -45,57 +45,60 @@ function AvatarComponent() {
     setIsSelected(src);
   };
   return (
-    <div className="w-11/12 md:w-7/12 mx-auto mt-10">
-      <div className="text-center lg:mt-8 mt-6">
-        <p className="text-base-white font-lexend leading-7 tracking-normal font-normal text-xl">
-          Pick Your Avatar
-        </p>
-        <p className="text-base-white font-roboto text-xs font-medium leading-4 tracking-[-0.12px]">
-          Pick an avatar that matches your vibe
-        </p>
-      </div>
-      <div className="mt-5">
-        <div className="w-28 mx-auto border border-base-white rounded-full p-3 text-center bg-white/20">
-          {isSelected ? (
-            <Image src={isSelected} alt="avatar" width={90} height={90} />
-          ) : (
-            <Image
-              src="/assets/avatar/Avatar4.png"
-              alt="avatar"
-              width={90}
-              height={90}
-            />
-          )}
-        </div>
+    <>
+      {personalitySelected && (
+        <div className="w-11/12 md:w-7/12 mx-auto mt-10">
+          <div className="text-center lg:mt-8 mt-6">
+            <p className="text-base-white font-lexend leading-7 tracking-normal font-normal text-xl">
+              Pick Your Avatar
+            </p>
+            <p className="text-base-white font-roboto text-xs font-medium leading-4 tracking-[-0.12px]">
+              Pick an avatar that matches your vibe
+            </p>
+          </div>
 
-        {personalitySelected && (
-          <div className="flex gap-2 mt-8">
-            {avatars.map(({ id, src }) => (
-              <button
-                type="button"
-                onClick={() => handleClick(src)}
-                key={id}
-                className={
-                  src === isSelected
-                    ? "border border-[#FD429C] mx-auto rounded-full p-1"
-                    : "mx-auto rounded-full p-1"
-                }
-              >
-                <div
+          <div className="mt-5">
+            <div className="w-28 mx-auto border border-base-white rounded-full p-3 text-center bg-white/20">
+              {isSelected ? (
+                <Image src={isSelected} alt="avatar" width={90} height={90} />
+              ) : (
+                <Image
+                  src="/assets/avatar/Avatar4.png"
+                  alt="avatar"
+                  width={90}
+                  height={90}
+                />
+              )}
+            </div>
+
+            <div className="flex gap-2 mt-8">
+              {avatars.map(({ id, src }) => (
+                <button
+                  type="button"
+                  onClick={() => handleClick(src)}
+                  key={id}
                   className={
                     src === isSelected
-                      ? "border border-base-white mx-auto rounded-full p-2 bg-white/20"
-                      : "mx-auto rounded-full p-2 bg-white/20"
+                      ? "border border-[#FD429C] mx-auto rounded-full p-1"
+                      : "mx-auto rounded-full p-1"
                   }
                 >
-                  <Image src={src} alt="avatar" width={60} height={60} />
-                </div>
-              </button>
-            ))}
+                  <div
+                    className={
+                      src === isSelected
+                        ? "border border-base-white mx-auto rounded-full p-2 bg-white/20"
+                        : "mx-auto rounded-full p-2 bg-white/20"
+                    }
+                  >
+                    <Image src={src} alt="avatar" width={60} height={60} />
+                  </div>
+                </button>
+              ))}
+            </div>
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
