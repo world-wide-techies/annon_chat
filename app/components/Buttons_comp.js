04e41e128c82889575076cae17e9ec8f8f2c750c
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-21 19:09:01 +0000

    Monday, August 21, 2023 at 8:09 PM West Africa Standard Time

--- app/components/Buttons_comp.js
+++ app/components/Buttons_comp.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useIdentityContext } from "../lib/identityContext";
 
-function Buttons({ btnText, chatroomName, username, }) {
- 
+function Buttons({ btnText, chatroomName, username }) {
   const {
     personality,
     setPersonality,
     personalitySelected,
     setPersonalitySelected,
-    gender
+    gender,
   } = useIdentityContext();
   const [isSelected, setIsSelected] = useState(false);
   const character = btnText.split(" ")[0].toLowerCase();
   const disableBtn = !(chatroomName && username && gender);
- 
+
   const handleSelect = (e) => {
     e.preventDefault();
+
     if (personality.includes(character)) {
       setPersonality(personality.filter((char) => char !== character));
       setIsSelected(false);
@@ -23,9 +23,9 @@ function Buttons({ btnText, chatroomName, username, }) {
       if (personality.length < 4) {
         setPersonality([...personality, character]);
         setIsSelected(true);
-      } else if (personality.length === 4) {
-        setPersonalitySelected(true);
       }
+
+      if (personality.length >= 3) setPersonalitySelected(true);
     }
 
     console.log(personality);
@@ -34,9 +34,9 @@ function Buttons({ btnText, chatroomName, username, }) {
   return (
     <button
       onClick={handleSelect}
-      className={`relative bg-transparent border-2 border-[#DCDCE5] rounded-full px-2 py-1 md:px-4 md:py-2 md:text-[17px] text-sm ${
+      className={`relative  border-2 border-[#DCDCE5] rounded-full px-2 py-1 md:px-4 md:py-2 md:text-[17px] text-sm ${
         disableBtn ? "opacity-30 cursor-not-allowed" : "hover:bg-[#ffffff4d]"
-      }  ${isSelected && `bg-[#ffffff4d]`}`}
+      }  ${isSelected ? "bg-[#ffffff4d]" : "bg-transparent"}`}
       disabled={disableBtn}
     >
       {btnText}
--- app/components/Personalities_comp.js
+++ app/components/Personalities_comp.js
@@ -31,7 +31,7 @@ function Personaliies({ chatroomName, username }) {
         <div className="md:mt-8 mt-5 text-white text-center">
           <p className="text-xl">Pick Your Avatar</p>
           <p className="text-sm mb-2 md:mb-6">
-            To pick an avatar, choose <span className="text-[#FD429C]">3 </span>
+            To pick an avatar, choose <span className="text-[#FD429C]">4 </span>
             words that <br /> best describe you
           </p>
           <div className="space-x-3 space-y-2 md:space-x-5 md:space-y-3 w-full">
