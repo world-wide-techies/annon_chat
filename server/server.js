f12ff544508b5b2f8bb66111f10deb1e4e400f45
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-30 17:53:30 +0100

    updated socket server

--- server/server.js
+++ server/server.js
@@ -8,10 +8,11 @@ app.use(cors());
 
 dotenv.config();
 const server = http.createServer(app);
-
+console.log(process.env.NEXT_PUBLIC_CLIENT_URL)
 const io = new Server(server, {
+
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
