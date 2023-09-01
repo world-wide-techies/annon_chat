116e49cd7f96d53a18fe938120bde70ccdb0e103
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-09-01 12:42:13 +0100

    test

--- app/lib/socketContext.js
+++ app/lib/socketContext.js
@@ -5,7 +5,9 @@ import { io } from "socket.io-client";
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
-  const socket = io(process.env.NEXT_PUBLIC_SERVER_URL);
+  const socket = io(
+    "https://sockect-server.onrender.com"
+  );
 
   return (
     <SocketContext.Provider
