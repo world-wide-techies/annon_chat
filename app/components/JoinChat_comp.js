b3fdb503c265d8503d4ec9fa21b1c1f6483afef2
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-09-19 02:58:15 +0100

    Updated chatroom

--- app/components/JoinChat_comp.js
+++ app/components/JoinChat_comp.js
@@ -62,7 +62,7 @@ export default function JoinChatComp() {
       } else {
         socket?.emit("join_room", room);
 
-        router.push(`/${room}`);
+        router.push(`/room?queryParam=${room}`);
       }
     }
   };
